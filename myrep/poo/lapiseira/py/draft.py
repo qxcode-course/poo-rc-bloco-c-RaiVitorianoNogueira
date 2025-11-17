f043c7acac5a115bfa86daddf172f7821d5ed083
RaiVitorianoNogueira <rainogueira2019@gmail.com>
2025-11-17 01:39:49 +0000

    comecei a lapiseira

---
+++ myrep/poo/lapiseira/py/draft.py
@@ -0,0 +1,66 @@
+class Lapiseira:
+    def __init__(self, calibre : float):
+        self.calibre =  calibre
+        self.bico =  None
+        self.tambor:list[Grafite] = []
+
+
+    def insert(self, grafite: Grafite):
+        if not self.calibre:
+            print("fail: calibre incompatível")
+            return 
+        self.tambor.append(grafite)
+
+
+
+
+
+
+
+
+
+
+class Grafite:
+    def __init__(self, calibre : float, dureza: str, tamanho: int):
+        self.calibre = calibre
+        self.dureza = dureza
+        self.tamanho = tamanho
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    
+
+
+
+
+
+
+
+
+
+main()
+
+
